27eaead3bc38be2b3603b208d61bf2d8776ccb08
shashwat558 <jainshashwat528@gmail.com>
2024-11-28 00:43:26 +0530

    Created authMiddleware

---
+++ backend/src/middleware.ts
@@ -0,0 +1,34 @@
+import { Next } from "hono";
+import { verify } from "hono/jwt";
+
+export default async function authMiddleware(c: any, next:Next){
+    const adminEmail = c.env.ADMIN_EMAIL;
+    const adminPassword = c.env.ADMIN_PASSWORD;
+    
+    const authHeader = c.req.headers.get("authorozation");
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return c.json({message: "unauthorized"});
+    }
+    const token = authHeader.split(" ")[1];
+    
+    try {
+        const payload = await verify(token, c.env.JWT_SECRET);
+
+        if(payload.email === adminEmail && payload.password === adminPassword){
+            return next()
+
+        } else {
+            // Unauthorized access, send 403 status
+            return c.json({ message: "Access Denied" }, 403);
+        }
+        
+
+
+        
+    } catch (error) {
+        console.error(error); // Log error for debugging
+        return c.json({ message: "Invalid or expired token" }, 401);
+    }
+    
+
+}
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono";
+import authMiddleware from "../middleware";
 
 
 export const blogRouter = new Hono()
 
 
 
-blogRouter.post('/', (c) => {
+blogRouter.post('/', authMiddleware, (c) => {
     return c.text("Blog route")
 })
 
