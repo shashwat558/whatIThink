d4cf076901e9a9341334e6879f918daf03d6d526
shashwat558 <jainshashwat528@gmail.com>
2025-04-14 15:27:23 +0530

    Add Heading extension to Tiptap component with custom configuration

--- frontend/components/tiptap.tsx
+++ frontend/components/tiptap.tsx
@@ -7,7 +7,7 @@ import StarterKit from "@tiptap/starter-kit"
 import UnderLine from "@tiptap/extension-underline"
 import CodeBlockLowLight from "@tiptap/extension-code-block-lowlight"
 
-// import Heading from "@tiptap/extension-heading"
+import Heading from "@tiptap/extension-heading"
 import Link from "@tiptap/extension-link"
 import { Toolbar } from "./ToolBar"
 import HardBreak  from "@tiptap/extension-hard-break";
@@ -45,6 +45,13 @@ export default function Tiptap({
         immediatelyRender: false,
         extensions: [StarterKit.configure({
 
+        }), Heading.configure({
+            levels: [2],
+            HTMLAttributes: {
+                class: "text-xl font-bold",
+                
+            }
+            
         }),
         UnderLine,
         CodeBlockLowLight.configure({
